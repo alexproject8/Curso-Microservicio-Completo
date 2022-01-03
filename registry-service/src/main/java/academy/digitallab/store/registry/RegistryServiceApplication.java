ef49280699ee51671659ce93ffc80bbd237b5225
Alex <ale95rodrimo@gmail.com>
2022-01-03 14:10:01 +0100

    todo completo de nuevo

--- registry-service/src/main/java/academy/digitallab/store/registry/RegistryServiceApplication.java
+++ registry-service/src/main/java/academy/digitallab/store/registry/RegistryServiceApplication.java
@@ -2,8 +2,10 @@ package academy.digitallab.store.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class RegistryServiceApplication {
 
 	public static void main(String[] args) {
